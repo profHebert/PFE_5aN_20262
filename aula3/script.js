920b948a1fa3c4831a92f6e7f5b0ab2dbb6bb7de
Hebert Bratefixe Alquimim <hebert@uni9.pro.br>
2026-09-10 17:34:24 -0300

    Aula3

---
+++ aula3/script.js
@@ -0,0 +1,9 @@
+document.write("Valeu!!");
+alert("ALERTA \n !!!");
+console.log("Onde estou?");
+document.getElementById("saida").innerHTML="Funcionou?";
+document.getElementById("saida").innerHTML+="<hr>e agora?";
+document.querySelector(".teste").innerHTML="Classe";
+document.querySelector("#saida").innerHTML="ID";
+document.querySelector("h1").innerHTML+=" - Javascript";
+document.querySelector("h1").style.color="red";
